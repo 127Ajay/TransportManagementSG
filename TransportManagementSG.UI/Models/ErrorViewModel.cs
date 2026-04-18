44b1e0279363d96a56752a953a71102ff6f2f859
Vivek Pai <vivekpaitech@gmail.com>
2026-04-18 12:15:06 +0530

    Project Setup -- Vivek

---
+++ TransportManagementSG.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransportManagementSG.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
