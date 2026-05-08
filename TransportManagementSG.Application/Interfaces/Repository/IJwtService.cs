328f92f425eb425f92c9deb8345a627481008db8
sachin-hanchate <sachin201182@gmail.com>
2026-05-08 23:12:43 +0530

    JWT token implemetation

---
+++ TransportManagementSG.Application/Interfaces/Repository/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportManagementSG.Application.Interfaces.Repository
+{
+    public interface IJwtService
+    {
+        string GenerateToken(string username, string role);
+    }
+}
